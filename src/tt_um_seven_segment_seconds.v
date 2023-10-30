971e21ebd1fe44ab9499d4a3eeb07277a4f22250
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-10-29 22:08:14 -0700

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -24,7 +24,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     // external clock is 10MHz, so need 24 bit counter
     reg [23:0] second_counter;
-    reg [5:0] digit;
+    reg [3:0] digit;
 
     // if external inputs are set then use that as compare count
     // otherwise use the hard coded MAX_COUNT
@@ -45,7 +45,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
                 digit <= digit + 1'b1;
 
                 // only count from 0 to 16
-                if (digit == 16)
+                if (digit == 15)
                     digit <= 0;
 
             end else
